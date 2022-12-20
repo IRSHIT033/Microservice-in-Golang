8ae4d4a9d1fbf37f82da65d16218723cf8a7bfb9
IRSHIT033 <mukherjeeirshit50@gmail.com>
2022-12-20 20:47:44 +0530

    Updated the user model

--- server/normal_user_service/models/userModel.go
+++ server/normal_user_service/models/userModel.go
@@ -1,10 +1,44 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Email    string `gorm:"unique"`
-	Name     string
-	Password string
+	Email     string `gorm:"unique"`
+	Name      string
+	Password  string
+	Addresses []Address
+	Cart      []Product
+	Wishlist  []WishlistOfUser
+	Orders    []Order
+}
+type Address struct {
+	Street     string
+	PostalCode string
+	Country    string
+}
+
+type Product struct {
+	ID    string
+	Name  string
+	Price string
+	unit  int
+}
+
+type WishlistOfUser struct {
+	ID          string
+	Name        string
+	Description string
+	Available   bool
+	price       int
+}
+
+type Order struct {
+	ID     string
+	Amount string
+	Date   time.Time
 }
