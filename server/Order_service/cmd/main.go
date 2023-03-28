bf9cff2935b1ce7a2d921994610c15087c6a1517
Irshit Mukherjee <83210491+IRSHIT033@users.noreply.github.com>
2023-03-29 01:22:29 +0530

    Update main.go

--- server/Order_service/cmd/main.go
+++ server/Order_service/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"time"
+	"fmt"
 
 	routeV1 "github.com/IRSHIT033/E-comm-GO-/server/Order_service/api/route/v1"
 	"github.com/IRSHIT033/E-comm-GO-/server/Order_service/bootstrap"
@@ -15,7 +16,7 @@ func main() {
 	gin := gin.Default()
 	routerV1 := gin.Group("order")
 	timeout := time.Duration(24) * time.Second
-
+	fmt.Println("hello world")  
 	routeV1.Setup(db, timeout, routerV1)
 	go kafka_consumer.ConsumeCart(db)
 	gin.Run(":3002")
